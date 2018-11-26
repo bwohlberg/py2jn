b3adacef01b189ff1c59d8d0f17d1ec9962c5e1f
Brendt Wohlberg <brendt@ieee.org>
2018-11-25 17:39:02 -0700

    Extend setup script, including version info

--- py2nb/__init__.py
+++ py2nb/__init__.py
@@ -1,1 +1,5 @@
 from __future__ import absolute_import
+
+
+__version__ = '0.0.1b1'
+
--- setup.py
+++ setup.py
@@ -1,9 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
+from ast import parse
+
+
+name = 'py2nb'
+# See http://stackoverflow.com/questions/2058802
+with open('py2nb/__init__.py') as f:
+    version = parse(next(filter(
+        lambda line: line.startswith('__version__'),
+        f))).body[0].value.s
+
+py_modules = [name]
+
+longdesc = \
+"""
+py2nb is a small utility for turning python scripts into jupyter notebooks and
+convert module-level multiline (triple quote) string literals into markdown
+cells.
+"""
 
 setup(
-    name="py2nb",
-    description="Convert python script into jupyter notebook",
-    packages=["py2nb"],
-    author="Continuum Analytics, Inc.",
-    license='BSD',
+    name             = name,
+    version          = version,
+    py_modules       = py_modules,
+    description      = 'py2nb: convert python script to Jupyter notebook',
+    long_description = longdesc,
+    keywords         = ['Jupyter notebook'],
+    platforms        = 'Any',
+    license          = 'BSD',
+    url              = 'https://github.com/bwohlberg/py2nb',
+    author           = 'Siu Kwan Lam',
+    author_email     = None,
+    maintainer       = 'Brendt Wohlberg',
+    maintainer_email = 'brendt@ieee.org',
+    setup_requires   = [],
+    tests_require    = ['pytest', 'pytest-runner'],
+    install_requires = ['nbformat'],
+    extras_require   = {
+        'tests': ['pytest', 'pytest-runner']},
+    classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Topic :: Utilities',
+    ],
+    zip_safe = True
 )
